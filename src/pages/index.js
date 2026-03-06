4ee41dbc980e66b4222350016af0449bd970015d
nalonal <rionaldyrizqy@gmail.com>
2026-03-07 03:03:55 +0700

    update template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,6 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
   return <Redirect to="/tutorials/docs/intro" />;
 }
-
